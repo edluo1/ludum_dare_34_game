5dff8cdfe624892220ca765fe36ffecb5aeb230e
Eddy Luo <edluo1@bu.edu>
2015-12-12 02:17:40 -0800

    Initial commit of project files
    Will work on when I wake up tomorow

---
+++ Assets/Scripts/SphereControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereControl : MonoBehaviour {
+
+	float ballSize;
+	float ballInterval = 1f;
+
+	// Use this for initialization
+	void Start () {
+		ballSize = GetComponent<SphereCollider>().radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("up")) { // enlarge the sphere
+			ballSize += ballInterval;
+			transform.localScale = new Vector3(ballSize, ballSize, ballSize);
+		}
+		if (Input.GetKeyDown("down")) { // shrink the sphere
+			if (ballSize - ballInterval > 0) {
+				ballSize -= ballInterval;
+				transform.localScale = new Vector3(ballSize, ballSize, ballSize);
+			}
+		}
+	}
+}
